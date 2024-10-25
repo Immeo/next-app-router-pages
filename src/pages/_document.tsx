d0cae024449db15dc2e2fc5a1a9462f0dc206900
immeo <wayfarer245@gmail.com>
2024-10-25 17:09:07 +0300

    03-clear files and add css variables

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document';
+import { Noto_Sans_KR } from 'next/font/google';
+
+const notoSansKR = Noto_Sans_KR({
+	weight: ['300', '400', '500', '700'],
+	subsets: ['latin']
+});
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang='ru'>
+			<Head />
+			<body className={`${notoSansKR.className}`}>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
