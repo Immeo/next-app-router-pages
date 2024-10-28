7300926877bc474a938ce9146265ec3f5bdf11da
immeo <wayfarer245@gmail.com>
2024-10-28 18:03:52 +0300

    06-add DetailedHTMLProps

--- src/pages/components/Button/Button.tsx
+++ src/pages/components/Button/Button.tsx
@@ -2,10 +2,16 @@ import cn from 'classnames';
 import styles from './Button.module.css';
 import { IButtonProps } from './Button.props';
 
-const Button = ({ children, appearance }: IButtonProps) => {
+const Button = ({
+	children,
+	appearance,
+	className,
+	...props
+}: IButtonProps) => {
 	return (
 		<button
-			className={cn(styles.button, {
+			{...props}
+			className={cn(styles.button, className, {
 				[styles.primary]: appearance === 'primary',
 				[styles.ghost]: appearance === 'ghost'
 			})}
