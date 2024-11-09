8365b05c5e54fb76c8f61c540d39b971e21558b1
immeo <wayfarer245@gmail.com>
2024-11-09 19:14:12 +0300

    13-add styles to footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,19 @@
+import cn from 'classnames';
+import { format } from 'date-fns';
+import styles from './Footer.module.css';
 import { IFooter } from './Footer.props';
 
-function Footer({ className }: IFooter) {
-	return <div className={className}>Footer</div>;
+function Footer({ className, ...props }: IFooter) {
+	return (
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+			<a href='#' target='_blank'>
+				Пользовательское соглашение
+			</a>
+			<a href='#' target='_blank'>
+				Политика конфиденциальности
+			</a>
+		</footer>
+	);
 }
 export default Footer;
