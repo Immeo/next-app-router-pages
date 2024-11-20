348f34ffab76394169dff5afd8766b3c5ec6fff0
immeo <wayfarer245@gmail.com>
2024-11-20 20:14:34 +0300

    15-get data menu categories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
+import axios from 'axios';
 import Button from '../../components/Button/Button';
 import Htag from '../../components/Htag/Htag';
 import PTag from '../../components/PTag/PTag';
 import Rating from '../../components/Rating/Rating';
 import VariableTag from '../../components/VariableTag/VariableTag';
 import { useStateRating } from '../../hook/workState/useStateRating';
+import { IMenuItem } from '../../interface/menu.interface';
 import { withLayout } from '../../layout/Layout';
 
-function Home(): JSX.Element {
+function Home({ menu }: IHomeProps): JSX.Element {
 	const { rating, setRating } = useStateRating(3);
 
 	return (
@@ -38,8 +40,39 @@ function Home(): JSX.Element {
 			<VariableTag size='s' color='primary'>
 				5
 			</VariableTag>
+			<ul>
+				{menu.map((m: IMenuItem) => (
+					<li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+				))}
+			</ul>
 		</>
 	);
 }
 
 export default withLayout(Home);
+
+export const getStaticProps = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<IMenuItem[]>(
+		process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+		{
+			firstCategory
+		}
+	);
+	if (!menu) {
+		return {
+			notFound: true
+		};
+	}
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
+};
+
+interface IHomeProps extends Record<string, unknown> {
+	menu: IMenuItem[];
+	firstCategory: number;
+}
