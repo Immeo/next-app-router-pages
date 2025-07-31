9382d58f51c83013f600c0ee05135d3b4653e789
immeo <wayfarer245@gmail.com>
2025-07-31 13:16:21 +0300

    22-brought the sidebar to the desired appearance

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,10 +1,14 @@
+import cn from 'classnames';
 import Searching from '../../components/Searching/Search';
+import Logo from '../logo.svg';
 import { Menu } from '../Menu/Menu';
+import Style from './Sidebar.module.css';
 import { SidebarProps } from './Sidebar.props';
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
 	return (
-		<div {...props}>
+		<div className={cn(className, Style.sidebar)} {...props}>
+			<Logo className={Style.logo} />
 			<Searching />
 			<Menu />
 		</div>
