52754bc4f7c15a0b573250949b34bbc11277be73
immeo <wayfarer245@gmail.com>
2025-09-03 19:22:38 +0300

    67-add react-yandex-metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from 'next/app';
 import { Noto_Sans_KR } from 'next/font/google';
 import Head from 'next/head';
+import { Router } from 'next/router';
+import ym, { YMInitializer } from 'react-yandex-metrika';
 import '../styles/globals.css';
 
 const notoSansKr = Noto_Sans_KR({
 	subsets: ['latin']
+});
+
+Router.events.on('routeChangeComplete', (url: string) => {
+	if (typeof window !== 'undefined') {
+		ym('hit', url);
+	}
 });
 
 export default function App({
@@ -21,7 +29,13 @@ export default function App({
 					content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}
 				/>
 				<meta property='og:locale' content='ru_RU' />
+				<link rel='preconnect' href='https://mc.yandex.ru' />
 			</Head>
+			<YMInitializer
+				accounts={[]}
+				options={{ webvisor: true, defer: true }}
+				version='2'
+			/>
 			<div className={notoSansKr.className}>
 				<Component {...pageProps} />
 			</div>
